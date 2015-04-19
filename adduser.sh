2b6eb1b8b747a332ef383ce393a52946f60f1f1a
maximelenoir <lenoir.maxime0@gmail.com>
2015-04-19 13:03:59 +0200

    Create user db in adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 USERDB=data/users.db
+mkdir -p $(dirname $USERDB)
+touch $USERDB
 read -p 'Username: ' USERNAME
 cat $USERDB | cut -d, -f1 | grep -qE "^${USERNAME}$" && { echo "ERROR: User ${USERNAME} already exists"; exit 1; }
 read -s -p 'Password: ' PASSWORD1
